07ad1c1ad9246ab529478c78613fc11919ceab9b
jaywcjlove <398188662@qq.com>
2021-02-14 09:17:31 +0800

    chore: Modify kkt config. #23

--- .kktrc.ts
+++ .kktrc.ts
@@ -20,6 +20,24 @@ export default (conf: Configuration, env: string, options: LoaderConfOptions) =>
     VERSION: JSON.stringify(pkg.version),
   }));
 
+  conf.optimization = {
+    ...conf.optimization,
+    splitChunks: {
+      cacheGroups: {
+        reactvendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'react-vendor',
+          chunks: 'all',
+        },
+        prismjs: {
+          test: /[\\/]node_modules[\\/](prismjs)[\\/]/,
+          name: 'prismjs-vendor',
+          chunks: 'async',
+        },
+      }
+    }
+  }
+
   return conf;
 }
 
