90c9198b7ce44df552214fe24b5ae4bd6c57c065
jaywcjlove <398188662@qq.com>
2022-03-17 01:18:08 +0800

    website: fix dark theme issue.

--- website/App.tsx
+++ website/App.tsx
@@ -1,34 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import GitHubCorners from '@uiw/react-github-corners';
 import Github from '@uiw/react-shields/esm/github';
 import Npm from '@uiw/react-shields/esm/npm';
+import '@wcj/dark-mode';
 import logo from './logo.svg';
 import './App.css';
-import MarkdownPreview, { MarkdownPreviewRef } from '../';
+import MarkdownPreview from '../';
 import MDStr from '../README.md';
 
 let val = 1;
 
 const App = () => {
   const [value, setValue] = useState('');
-  const [theme, setTheme] = useState<'light' | 'dart'>('dart');
-  const ref = React.createRef<MarkdownPreviewRef>();
-
-  useEffect(() => {
-    const themelocal = (localStorage.getItem('theme') || 'dart') as 'light' | 'dart';
-    setTheme(themelocal);
-    document.body.setAttribute('data-color-mode', themelocal);
-  }, []);
-  function changeTheme() {
-    setTheme(theme === 'dart' ? 'light' : 'dart');
-    document.body.setAttribute('data-color-mode', theme === 'dart' ? 'light' : 'dart');
-    localStorage.setItem('theme', theme === 'dart' ? 'light' : 'dart');
-  }
   return (
     <div className="App">
+      <dark-mode permanent light="Light" dark="Dark"></dark-mode>
       <GitHubCorners zIndex={9999} fixed target="__blank" href="https://github.com/uiwjs/react-markdown-preview" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img style={{}} src={logo} className="App-logo" alt="logo" />
         <a href="https://github.com/uiwjs/react-markdown-preview" target="_blank" rel="noopener noreferrer">
           React Markdown Preview
         </a>
@@ -46,12 +35,7 @@ const App = () => {
           spellCheck="false"
           onChange={(e) => setValue(e.target.value)}
         />
-        <MarkdownPreview ref={ref} className="App-editor-preview" source={value} />
-        <label className="App-theme">
-          {theme === 'dart' ? '🌒' : '🌞'}
-          <input type="checkbox" checked={theme === 'dart'} onChange={(env) => changeTheme()} />
-          {theme === 'dart' ? 'Dart' : 'Light'}
-        </label>
+        <MarkdownPreview className="App-editor-preview" source={value} />
       </div>
       <MarkdownPreview className="App-markdown" source={MDStr.replace(/([\s\S]*)<!--dividing-->/, '')} />
       <div className="App-footer">
