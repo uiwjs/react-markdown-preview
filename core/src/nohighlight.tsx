2694e00b0f5fd667178b8626fb23375df09368ae
Momo <github2@5208866.xyz>
2025-07-28 13:49:19 +0300

    fixed skipHTML bug

--- core/src/nohighlight.tsx
+++ core/src/nohighlight.tsx
@@ -3,7 +3,6 @@ import MarkdownPreview from './preview';
 import { type PluggableList } from 'unified';
 import rehypeRewrite from 'rehype-rewrite';
 import rehypeAttrs from 'rehype-attr';
-import rehypeRaw from 'rehype-raw';
 import { reservedMeta } from './plugins/reservedMeta';
 import { retrieveMeta } from './plugins/retrieveMeta';
 import { rehypeRewriteHandle, defaultRehypePlugins } from './rehypePlugins';
@@ -14,7 +13,6 @@ export * from './Props';
 export default React.forwardRef<MarkdownPreviewRef, MarkdownPreviewProps>((props, ref) => {
   const rehypePlugins: PluggableList = [
     reservedMeta,
-    rehypeRaw,
     retrieveMeta,
     ...defaultRehypePlugins,
     [rehypeRewrite, { rewrite: rehypeRewriteHandle(props.disableCopy ?? false, props.rehypeRewrite) }],
